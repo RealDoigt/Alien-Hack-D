ea67155665d8fce439fad77c1fcca0a53ffc3643
Doigt <57451013+RealDoigt@users.noreply.github.com>
2022-06-14 23:43:34 -0400

    Update level.d
    réorganisation du code afin d'en alléger la lecture

--- data/source/game/level.d
+++ data/source/game/level.d
@@ -1,36 +1,7 @@
 module game.level;
 import std.string;
-import game.bits;
 import raylib;
-
-class LevelSizeTooSmallException : Exception
-{
-    this(string file)
-    {
-        super("The level %s is too small".format(file), __FILE__, __LINE__);
-    }
-}
-
-class LevelFileNotFoundException : Exception
-{
-    this(string file)
-    {
-        super("The level %s couldn't be found".format(file), __FILE__, __LINE__);
-    }
-}
-
-class InvalidBitTypeException : Exception
-{
-    this(ubyte value)
-    {
-        super
-        (
-            "Number %d doesn't correspond to an extant bit type".format(value), 
-            __FILE__, 
-            __LINE__
-        );
-    }
-}
+import game;
 
 class Level
 {
@@ -139,5 +110,11 @@ class Level
             nots,
             concatBitsToString
         );
+    }
+
+    void toggle(int x, int y, bool activateEffect = true)
+    {
+        bits[y][x].isAlive = !bits[y][x].isAlive;
+        if (activateEffect) bits[y][x].change(this, x, y);
     }
 }
