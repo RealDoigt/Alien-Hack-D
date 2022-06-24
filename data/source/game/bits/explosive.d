d56cc074dbfd3b913a72b21179c81279192d0fa6
Doigt <57451013+RealDoigt@users.noreply.github.com>
2022-06-24 16:41:14 -0400

    Update explosive.d

--- data/source/game/bits/explosive.d
+++ data/source/game/bits/explosive.d
@@ -1,10 +1,22 @@
 module game.bits.explosive;
 import game.bits.bit;
+import game.level;
+
 
 class Explosive : Bit
 {
     this (bool isAlive)
     {
         super(isAlive);
+    }
+
+    override void change(Level level, int x, int y)
+    {
+        if (level.isAlive(x + 1, y) && level.isAlive(x - 1, y))
+        {
+            isAlive = false;
+            level.toggle(x + 1, y, false);
+            level.toggle(x - 1, y, false);
+        }
     }
 }
