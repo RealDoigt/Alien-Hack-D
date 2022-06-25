4d8010b3bae49c40e9b66248ed697db21aad50f9
Doigt <57451013+RealDoigt@users.noreply.github.com>
2022-06-25 11:37:23 -0400

    Update wise.d
    ajout de la fonction change

--- data/source/game/bits/wise.d
+++ data/source/game/bits/wise.d
@@ -1,10 +1,16 @@
 module game.bits.wise;
 import game.bits.bit;
+import game.level;
 
 class Wise : Bit
 {
     this (bool isAlive)
     {
         super(isAlive);
+    }
+
+    override void change (Level level, int x, int y)
+    {
+        isAlive = level.getSolution[x] == '1';
     }
 }
