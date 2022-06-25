c5a2deb059bc45dcaebe5e58b49b15442fee5c25
Doigt <57451013+RealDoigt@users.noreply.github.com>
2022-06-25 11:37:13 -0400

    Update virus.d
    ajout de la fonction change

--- data/source/game/bits/virus.d
+++ data/source/game/bits/virus.d
@@ -1,10 +1,21 @@
 module game.bits.virus;
 import game.bits.bit;
+import game.level;
 
 class Virus : Bit
 {
     this (bool isAlive)
     {
         super(isAlive);
+    }
+
+    override void change (Level level, int x, int y)
+    {
+        isAlive = true;
+
+        if (!level.isAlive(x + 1, y)) level.toggle(x + 1, y);
+        if (!level.isAlive(x - 1, y)) level.toggle(x - 1, y);
+        if (!level.isAlive(x, y + 1)) level.toggle(x, y + 1);
+        if (!level.isAlive(x, y - 1)) level.toggle(x, y - 1);
     }
 }
