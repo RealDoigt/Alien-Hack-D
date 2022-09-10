7716b1fa0b4e1e0342ba05fa68a921f4d44d2155
RealDoigt <57451013+RealDoigt@users.noreply.github.com>
2022-09-10 16:12:24 -0400

    bit color logic
    + added necessary imports
    + added a color field which is green for a normal bit
    + added a getColor function which returns the value of the color field if not dead else it returns a darkened version of the color field

--- data/source/game/bits/bit.d
+++ data/source/game/bits/bit.d
@@ -1,10 +1,14 @@
 module game.bits.bit;
+import raylib_misc;
 import game.level;
+import raylib;
 
 
 class Bit
 {
     bool isAlive;
+    protected auto color = green;
+    private const dark = 3; // the param which is used to darken the color of dead bits
 
     this(bool isAlive)
     {
@@ -13,4 +17,6 @@ class Bit
 
     // Keep it empty because the regular bit doesn't do anything.
     void changeState(Level level, int x, int y){}
+
+    Color getColor() => isAlive ? color : Color(color.r >> dark, color.g >> dark, color.b >> dark);
 }
