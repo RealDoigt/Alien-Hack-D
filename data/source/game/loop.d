15fa9591bc5c534e0008d663f8aa89fa756f84e3
RealDoigt <57451013+RealDoigt@users.noreply.github.com>
2022-09-10 17:15:13 -0400

    basic game screen display

--- data/source/game/loop.d
+++ data/source/game/loop.d
@@ -1,48 +1,15 @@
 module game.loop;
+import raylib;
+import game;
 
-
+enum startX    = 100;
+enum startY    = 50;
+enum bitWidth  = 20;
+enum bitHeight = 60;
 
 void displayGameplay()
 {
-    for (int y = level.height - 1; y >= 0; --y)
-    {
-        for (int x = 0; x < LEVEL_MAX; ++x)
-        {
-            Color color;
-
-            switch (level.bits[y][x].type)
-            {
-                case NORMAL:
-                    color = GREEN;
-                    break;
-
-                case EXPLOSIVE:
-                    color = RED;
-                    break;
-
-                case VIRAL:
-                    color = PURPLE;
-                    break;
-
-                case ZOMBIE:
-                    color = YELLOW;
-                    break;
-
-                case WISE:
-                    color = WHITE;
-                    break;
-
-                default: return;
-            }
-
-            if (!level.bits[y][x].is_alive)
-            {
-                color.r >>= 3;
-                color.g >>= 3;
-                color.b >>= 3;
-            }
-
-            DrawRectangle(START_X + (x << 5), START_Y + y * 100, BIT_WIDTH, BIT_HEIGHT, color);
-        }
-    }
+    for (int y = activeLevel.height - 1; y >= 0; --y)
+        for (int x = 0; x < activeLevel.width; ++x)
+            DrawRectangle(startX + (x << 5), startY + y * 100, bitWidth, bitHeight, activeLevel.getColor(x, y));
 }
