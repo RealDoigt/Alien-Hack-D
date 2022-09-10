0f6fc98e18a751b01a314f253213ec15346c7582
RealDoigt <57451013+RealDoigt@users.noreply.github.com>
2022-09-10 17:16:25 -0400

    Update app.d
    new test main

--- data/source/app.d
+++ data/source/app.d
@@ -7,16 +7,15 @@ void main()
     InitWindow(1080, 720, "A raylib game");
     SetTargetFPS(60);
 
-    auto level = new Level("test.dat");
+    activeLevel = new Level("test.dat");
 
     while (!WindowShouldClose)
     {
         BeginDrawing;
         scope (exit) EndDrawing;
         
-        // code de test
-        (level.toString ~ "\0").ptr.DrawText(0, 0, 8, black);
+        displayGameplay;
 
-        white.ClearBackground;
+        black.ClearBackground;
     }
 }
