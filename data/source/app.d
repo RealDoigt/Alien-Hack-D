f2b21375748bc45f2b40411666cead4d04d437bf
RealDoigt <57451013+RealDoigt@users.noreply.github.com>
2022-11-05 23:27:37 -0400

    Update app.d
    ramener le niveau test la ou il doit être

--- data/source/app.d
+++ data/source/app.d
@@ -2,13 +2,18 @@ import raylib_misc;
 import raylib;
 import game;
 import menu;
+import global;
+import std.stdio;
 
 void main()
 {
     InitWindow(1080, 720, "A raylib game");
     SetTargetFPS(60);
 
-    auto level = new Level("test.dat");
+    activeLevel = new Level("test.dat");
+
+    andButton = new ClickableImage(0, 0, "and".getImagePath, "and_on_hover".getImagePath, "and_is_clicked".getImagePath);
+    xorButton = new ClickableImage(0, 0, "xor".getImagePath, "xor_on_hover".getImagePath, "xor_is_clicked".getImagePath);
 
     while (!WindowShouldClose)
     {
