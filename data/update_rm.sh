b2ea7b7f4329e5474a3140a8ceda117b28db5dd6
RealDoigt <57451013+RealDoigt@users.noreply.github.com>
2022-12-23 16:30:32 -0500

    fixed typo

--- data/update_rm.sh
+++ data/update_rm.sh
@@ -5,9 +5,9 @@ location='https://github.com/RealDoigt/raylib_misc/archive/refs/heads/'
 # check if the branch argument is set
 if [ -z $1 ];
 then
-    location="$location$1"
-    else
     location="${location}main.zip"
+    else
+    location="$location$1"
 fi
 
 wget -O temporary.zip $location
