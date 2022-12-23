2964df76d3280e0dc41c28ef30115457236fff5c
RealDoigt <57451013+RealDoigt@users.noreply.github.com>
2022-12-23 16:23:11 -0500

    Update update_rm.sh
    location set

--- data/update_rm.sh
+++ data/update_rm.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
+location='https://github.com/RealDoigt/raylib_misc/archive/refs/heads/'
+
 # check if the branch argument is set
 if [ -z $1 ];
 then
+    location="$location$1"
+    else
+    location="${location}main.zip"
 fi
-    
