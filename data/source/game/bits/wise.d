ffff54d78e0fd7e68eceab4d229e802d44962cf6
RealDoigt <57451013+RealDoigt@users.noreply.github.com>
2023-05-20 13:35:34 -0400

    wise bit colour

--- data/source/game/bits/wise.d
+++ data/source/game/bits/wise.d
@@ -1,12 +1,15 @@
 module game.bits.wise;
 import game.bits.bit;
+import raylib_misc;
 import game.level;
+import raylib;
 
 class Wise : Bit
 {
     this (bool isAlive)
     {
         super(isAlive);
+        color = white;
     }
 
     override void changeState(Level level, int x, int y)
