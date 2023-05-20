e202eda25495b92b6dc3ad2f52b953b94745e331
RealDoigt <57451013+RealDoigt@users.noreply.github.com>
2023-05-20 08:29:59 -0400

    colour normal bit

--- data/source/game/bits/bit.d
+++ data/source/game/bits/bit.d
@@ -1,10 +1,13 @@
 module game.bits.bit;
+import raylib_misc;
 import game.level;
-
+import raylib;
 
 class Bit
 {
     bool isAlive;
+    protected auto color = green;
+    private const dark = 3; // the param which is used to darken the color of dead bits
 
     this(bool isAlive)
     {
@@ -13,4 +16,9 @@ class Bit
 
     // Keep it empty because the regular bit doesn't do anything.
     void changeState(Level level, int x, int y){}
+
+    Color getColor()
+    {
+        return isAlive ? color : Color(color.r >> dark, color.g >> dark, color.b >> dark, color.a);
+    }
 }
