508bd8533415b47c513fe4e1c40ed472eca4141c
RealDoigt <57451013+RealDoigt@users.noreply.github.com>
2023-07-13 18:37:46 -0400

    init level and buttons

--- data/source/app.d
+++ data/source/app.d
@@ -8,7 +8,10 @@ void main()
     InitWindow(1080, 720, "A raylib game");
     SetTargetFPS(60);
 
-    auto level = new Level("test.dat");
+    activeLevel = new Level("test.dat");
+
+    andButton = new ClickableImage(0, 0, "and".getImagePath, "and_on_hover".getImagePath, "and_is_clicked".getImagePath);
+    xorButton = new ClickableImage(0, 0, "xor".getImagePath, "xor_on_hover".getImagePath, "xor_is_clicked".getImagePath);
 
     while (!WindowShouldClose)
     {
