9c60f0bfce894dcddf9caea3df1b2bb774814d98
RealDoigt <57451013+RealDoigt@users.noreply.github.com>
2023-08-25 18:11:51 -0400

    save function

--- data/source/game/io.d
+++ data/source/game/io.d
@@ -3,5 +3,12 @@ import game;
 
 void save(Score[] scores, string fileName)
 {
-    
+    ubyte[] binaryData;
+
+    foreach (c; currentModule) binaryData ~= cast(ubyte)c;
+    binaryData ~= cast(ubyte)scores.length;
+
+    foreach (s; scores)
+      foreach (b; s.getBytes)
+        binaryData ~= b;
 }
